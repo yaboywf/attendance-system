0331af360c85c387b343793e01b385546b9b9750
Dylan <dylanyeowf@gmail.com>
2025-03-11 18:07:43 +0800

    Added test

--- frontend/src/tests/login_page.test.js
+++ frontend/src/tests/login_page.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import LoginPage from '../src/LoginPage';
+import LoginPage from '../LoginPage';
 
 test('renders login page', () => {
 	render(<LoginPage />);
